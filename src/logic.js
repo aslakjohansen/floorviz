9a11fc7aa0145579d521b2268ed3099f15751871
Aslak Johansen <aslakj@gmail.com>
2017-12-26 21:52:15 +0100

    Cleanup

--- src/logic.js
+++ src/logic.js
@@ -3,13 +3,8 @@ window.onload=function(){
     //var id = svgdoc.getElementById("path817")
 
     var o = document.getElementById("building");
-    console.log(o);
     var svgdoc = o.contentDocument;
-    console.log(svgdoc);
     var item = svgdoc.getElementById("path817");
-    console.log(item);
-    //var id = svgdoc.getElementById("encoded:floor=0,room=17,name=fox,type=shape")
-//    item.setAttribute("fill", "red");
     
     // read
     var attrs = item.getAttribute("style").split(";");
@@ -29,7 +24,5 @@ window.onload=function(){
         attrs += key+":"+d[key];
     }
     item.setAttribute("style", attrs);
-    
-    console.log(item);
 }
 
