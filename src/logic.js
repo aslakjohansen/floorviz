0f49890067c267a74687881e3018b9d2379908f1
Aslak Johansen <aslakj@gmail.com>
2017-12-27 13:51:43 +0100

    Got initial parsing working

--- src/logic.js
+++ src/logic.js
@@ -1,3 +1,5 @@
+var index = {};
+
 colorize = function (tag, color) {
     // read
     var attrs = tag.getAttribute("style").split(";");
@@ -28,7 +30,31 @@ window.onload = function () {
     ids = svgdoc.getElementsByTagName('*');
     console.log(ids);
     
-    ids = svgdoc.querySelectorAll('[id^=encoded]');
-    console.log(ids);
+    tags = svgdoc.querySelectorAll('[id^=encoded]');
+    console.log(tags);
+    for (var i=0 ; i<tags.length; i++) {
+        tag = tags[i];
+        elements = tag.getAttribute("id").split(":");
+        if (elements.length != 2 || elements[0]!="encoded") {
+            console.log("error in id match (len="+(elements.length)+", element0='"+elements[0]+"'):");
+            console.log(tag);
+        }
+        
+        d = {}
+        pairs = elements[1].split(",");
+        for (var j=0 ; j<pairs.length; j++) {
+            parts = pairs[j].split("=");
+            d[parts[0]] = parts[1];
+        }
+        if ("room" in d) {
+            console.log("inside");
+            index[d["room"]] = {
+                "tag": tag,
+                "data": d,
+            }
+        }
+    }
+    
+    console.log(index);
 }
 
