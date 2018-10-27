b60004b80cdf04b5ff0e07450b717077b2e5f47b
Aslak Johansen <aslakj@gmail.com>
2018-10-28 01:01:25 +0200

    Added processing of the modalities query

--- src/logic.js
+++ src/logic.js
@@ -4,6 +4,9 @@ var floor2svg = {};
 var room2path = {};
 var room2type = {};
 var room2area = {};
+var uuid2room = {};
+var uuid2modality = {};
+var archiver2uuids = {};
 //var index = {}; // roomname ↦ {"tag" ↦ TAG, "data" ↦ KEY ↦ VALUE}
 //var uuid2roomname = {}; // uuid ↦ roomname
 
@@ -36,7 +39,7 @@ hoddb_query = function (hoddb_url, query_url, callback) {
 }
 
 new_config = function (hoddb_url, callback) {
-    var count = 2;
+    var count = 3;
     
     // floor2svg
     hoddb_query(hoddb_url, 'queries/floor2svg.rq', function (data) {
@@ -60,6 +63,25 @@ new_config = function (hoddb_url, callback) {
         if (--count==0 && callback) callback();
     });
     
+    // uuid2room, archiver2uuids, uuid2modality
+    hoddb_query(hoddb_url, 'queries/modalities.rq', function (data) {
+        var i;
+        for (i=0 ; i<data.length ; i++) {
+            entry = data[i];
+            modality = entry["?modality"]["Value"];
+            uuid     = entry["?uuid"]["Value"];
+            room     = entry["?name"]["Value"];
+            url      = entry["?url"]["Namespace"]+":"+entry["?url"]["Value"];
+            uuid2room[uuid] = room;
+            if (!archiver2uuids.hasOwnProperty(url)) {
+                archiver2uuids[url] = [];
+            }
+            archiver2uuids[url].push(uuid);
+            uuid2modality[uuid] = modality;
+        };
+        if (--count==0 && callback) callback();
+    });
+    
 }
 
 window.onload = function () {
@@ -70,6 +92,7 @@ window.onload = function () {
             console.log("got enter and '"+node.value+"'")
             hod_uri = node.value;
             new_config(hod_uri, function () {
+                console.log(archiver2uuids);
                 console.log("Ready");
             });
         }
