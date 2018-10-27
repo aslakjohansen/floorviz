cfff83682c69e8aacdafb0b9a1410ffb70a099d5
Aslak Johansen <aslakj@gmail.com>
2018-10-28 00:46:29 +0200

    Parsed the results of room.rq

--- src/logic.js
+++ src/logic.js
@@ -1,6 +1,9 @@
 var useragent = "floorviz";
 
 var floor2svg = {};
+var room2path = {};
+var room2type = {};
+var room2area = {};
 //var index = {}; // roomname ↦ {"tag" ↦ TAG, "data" ↦ KEY ↦ VALUE}
 //var uuid2roomname = {}; // uuid ↦ roomname
 
@@ -35,6 +38,9 @@ hoddb_query = function (hoddb_url, query_url, callback) {
 }
 
 new_config = function (hoddb_url, callback) {
+    var count = 2;
+    
+    // floor2svg
     hoddb_query(hoddb_url, 'queries/floor2svg.rq', function (data) {
         console.log("data fetched:");
         console.log(data)
@@ -45,7 +51,25 @@ new_config = function (hoddb_url, callback) {
             floor2svg[entry["?floorname"]["Value"]] = entry["?floorplan"]["Namespace"]+":"+entry["?floorplan"]["Value"];
         };
         console.log(floor2svg);
-        if (callback) callback();
+        if (--count==0 && callback) callback();
+    });
+    
+    // room2path, room2type and room2area
+    hoddb_query(hoddb_url, 'queries/room.rq', function (data) {
+        console.log("data fetched:");
+        console.log(data)
+        var i;
+        for (i=0 ; i<data.length ; i++) {
+            entry = data[i];
+            console.log(entry)
+            room2path[entry["?name"]["Value"]] = entry["?path"]["Value"];
+            room2type[entry["?name"]["Value"]] = entry["?type"]["Value"];
+            room2area[entry["?name"]["Value"]] = entry["?area"]["Value"];
+        };
+        console.log(room2path);
+        console.log(room2type);
+        console.log(room2area);
+        if (--count==0 && callback) callback();
     });
     
 }
