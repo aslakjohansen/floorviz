8eb2282d82b3fe11c6d695dac00ef82623b170d4
Aslak Johansen <aslakj@gmail.com>
2018-11-05 23:46:56 +0800

    Cleanup

--- src/logic.js
+++ src/logic.js
@@ -16,7 +16,6 @@ var modality_max = {} // modality ↦ maximum value
 // colormap range
 minhue = 0
 maxhue = 85.0
-//maxhue = 85.0/360
 
 fetch_data = function (url, callback) {
     var xhr = new XMLHttpRequest();
@@ -75,7 +74,6 @@ new_config = function (hoddb_url, callback) {
     // uuid2room, archiver2uuids, uuid2modality
     hoddb_query(hoddb_url, 'queries/modalities.rq', function (data) {
         var i;
-        console.log("archiver2uuids (pre) = "+Object.keys(archiver2uuids));
         for (i=0 ; i<data.length ; i++) {
             var entry    = data[i];
             var modality = entry["?modality"]["Value"];
@@ -88,10 +86,7 @@ new_config = function (hoddb_url, callback) {
             }
             archiver2uuids[url].push(uuid);
             uuid2modality[uuid] = modality;
-            console.log("archiver2uuids = "+Object.keys(archiver2uuids));
         };
-        console.log("archiver2uuids (post) = "+Object.keys(archiver2uuids));
-        console.log(archiver2uuids);
         if (--count===0 && callback) callback();
     });
     
@@ -105,20 +100,13 @@ new_view = function () {
     for (var i=0 ; i<floors.length ; i++) {
         var svg = floor2svg[floors[i]];
         var modality2obj = svg2obj[svg];
-        console.log(svg2obj);
-        console.log(modality2obj);
         var modalities = Object.keys(modality2obj);
-        console.log("new_view - modalities:"+modalities);
         for (var j=0 ; j<modalities.length ; j++) {
             var obj = modality2obj[modalities[j]];
-            console.log("floors["+i+"]="+floors[i]+" fvalue="+fvalue+" modalities["+j+"]="+modalities[j]+" mvalue="+mvalue);
             if (floors[i]===fvalue && modalities[j]===mvalue) {
-                console.log("!!!!!!!!!!!!!!!!!!!!!!!!!! inside");
                 obj.width = "100%";
-//                obj.style.display = "block";
             } else {
                 obj.width = "0%";
-//                obj.style.display = "none";
             }
         }
     }
@@ -152,35 +140,22 @@ construct_ui = function (callback) {
     
     // populate floormap
     var floors = Object.keys(floor2svg);
-    console.log(">>>>>floors="+floors);
-    console.log(">>>>>modalities="+modalities);
     for (var i=0 ; i<floors.length ; i++) {
         var f = floors[i];
-        console.log("floor:"+f);
         var svg = floor2svg[f];
         for (var j=0 ; j<modalities.length ; j++) {
             var modality = modalities[j];
-            console.log("modality:"+modality);
             var identifier = f+modality;
-            console.log("identifier"+identifier);
             var obj = document.createElement("object");
-//            obj.setAttribute("id"   , f);
             obj.setAttribute("class", "svgClass");
             obj.setAttribute("id", identifier);
             obj.setAttribute("type" , "image/svg+xml");
             obj.setAttribute("data" , svg);
-//            obj.setAttribute("width", "100%");
             obj.setAttribute("width", "0%");
-//            obj.style.display = "block"; //"none";
-            console.log("obj1 properties: '"+Object.getOwnPropertyNames(obj)+"'");
             document.getElementById("floormap").appendChild(obj);
-            console.log("obj2 properties: '"+Object.getOwnPropertyNames(obj)+"'");
-            console.log(svg+"/"+modality+" <- "+obj);
             if (!svg2obj.hasOwnProperty(svg)) svg2obj[svg] = {};
             document.getElementById("building")
             svg2obj[svg][modality] = document.getElementById(identifier);
-//            svg2obj[svg][modality] = obj;
-            console.log("obj3 properties: '"+Object.getOwnPropertyNames(obj)+"'");
             if (identifier === "Floor 2CO2 Sensor") {
                 obj.onload = function () {
                     console.log("object loaded");
@@ -205,17 +180,14 @@ float2hex = function (float) {
 };
 
 rgb2hexcode = function(r,g,b) {
-  return "#"+float2hex(r)+float2hex(g)+float2hex(b);
+    return "#"+float2hex(r)+float2hex(g)+float2hex(b);
 };
 
 // https://en.wikipedia.org/wiki/HSL_and_HSV
 hsv2color = function (h, s, v) {
     var c = s*v;
-    console.log("chroma = "+c);
     var hmark = h/60;
-    console.log("hmark = "+hmark);
     var x = c*(1 - Math.abs((hmark % 2)-1));
-    console.log("x = "+x);
     var r1, g1, b1;
     switch (Math.floor(hmark)) {
         case 0: r1 = c, g1 = x, b1 = 0; break;
@@ -225,24 +197,17 @@ hsv2color = function (h, s, v) {
         case 4: r1 = x, g1 = 0, b1 = c; break;
         case 5: r1 = c, g1 = 0, b1 = x; break;
     }
-    console.log("r1g1b1 ("+r1+","+g1+","+b1+")");
     var m = v-c;
-    console.log("m = "+m);
     var r = r1+m;
     var g = g1+m;
     var b = b1+m;
-    console.log("rgb ("+r+","+g+","+b+")");
     
     return rgb2hexcode(r, g, b);
-//    return "#cc3300";
 }
 
 colorize = function (f, modality, path, value) {
-    console.log(f+"/"+modality+"["+path+"] <- "+value);
-    
     svg = floor2svg[f];
     obj = svg2obj[svg][modality];
-    console.log("object is "+obj);
     
     // update map of lowest and highest observed values
     if (!modality_min.hasOwnProperty(modality)) modality_min[modality] = value;
@@ -256,26 +221,12 @@ colorize = function (f, modality, path, value) {
     pos = (value-vmin)/(vmax-vmin);
     if (isNaN(pos)) pos = 0.5; // hack to make first value not result in a divide-by-zero
     hue = minhue+(1.0-pos)*(maxhue-minhue);
-    console.log("vmin="+vmin+" vmax="+vmax+" pos="+pos+" hue="+hue);
     color = hsv2color(hue, 1, 0.85);
-    console.log("color = "+color);
-    
-    console.log("oneliner-2: "+document.getElementById("Floor 2CO2 Sensor"));
-    console.log("oneliner-1: "+document.getElementById("Floor 2CO2 Sensor").contentDocument);
-    console.log("oneliner: "+document.getElementById("Floor 2CO2 Sensor").contentDocument.getElementById("path305"));
     
     // locate path
-//    var svgdoc = obj.contentDocument;
-//    var svgdoc = obj;
     var svgdoc = document.getElementById(f+modality).contentDocument;
-//    var svgdoc = document.getElementById("path305");
-    console.log(obj);
-    console.log("svgdoc = "+svgdoc);
-    console.log("properties: '"+Object.getOwnPropertyNames(svgdoc)+"'");
     if (svgdoc === null) return;
-    console.log("path="+path);
     var p = svgdoc.getElementById(path);
-    console.log("p = "+p);
     
     // read
     var attrs = p.getAttribute("style").split(";");
@@ -329,28 +280,20 @@ subscribe = function (callback) {
     var archivers = Object.keys(archiver2uuids);
     for (var i=0 ; i<archivers.length ; i++) {
         var archiver = archivers[i];
-//        var archiver = "http://localhost/volta/republish";
         var uuids = archiver2uuids[archiver];
-        console.log("About to subscribe to "+archiver+" with uuids "+uuids);
         
         var xhr = new XMLHttpRequest();
         xhr.open('POST', archiver, true); // true for asynchronous
-//        xhr.setRequestHeader("User-Agent", useragent);
         xhr.seenBytes = 0;
         
         xhr.onreadystatechange = function() {
-//            console.log("recv: '"+xhr.readyState+"'");
             if(xhr.readyState > 2) {
                 var json_strings = xhr.responseText.substr(xhr.seenBytes).split("\n");
-//                console.log("elements = "+json_strings.length);
-//                console.log(json_strings);
                 for (var i=0 ; i<json_strings.length; i++) {
                     json_string = json_strings[i];
-//                    console.log(json_string);
                     try {
                         o = JSON.parse(json_string);
                         if (o !== null) process(o, null);
-//                        console.log(o);
                     } catch (err) {
                         console.log("Error: "+err);
                     }
@@ -359,13 +302,8 @@ subscribe = function (callback) {
             }
         };
         
-//        where_clause = uuids.map(function (uuid) { return "uuid="+uuid}).join(" or ");
         where_clause = uuids.map(function (uuid) { return "uuid=\""+uuid+"\""}).join(" or ");
-        console.log("where_clause: "+where_clause);
-        
         xhr.send(where_clause);
-//        xhr.send("Metadata/Location/Building=\"OU44\"");
-//        xhr.send("Metadata/Location/Building=\"OU44\" and Metadata/Media=\"air\"");
     }
     
     if (callback) callback();
